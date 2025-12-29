8db81350b7820ade4c186169916b8d2b1c9a6472
Ganesh <ganeshmkn28@gmail.com>
2025-12-29 18:13:36 +0530

    added date to new transaction

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 const form=document.getElementsByTagName("form");
 const source=document.getElementById("new-source");
 const amount=document.getElementById("new-amount");
+const dateTime=document.getElementById("dateTime");
 let transaction_list=document.getElementById("trans-list")
 function handleFormSubmit(event) {
     event.preventDefault();
-    transaction_list.appendChild(newTransaction(source.value, amount.value))
+    let newDate=new Date(dateTime.value);
+    let textDate=newDate.getDate()+"-"+newDate.getMonth()+"-"+newDate.getFullYear();
+
+    transaction_list.appendChild(newTransaction(source.value, amount.value, textDate))
 }
  
-function newTransaction(tname, tamt) {
+function newTransaction(tname, tamt, textDate) {
+    // empty transaction item
     const TransactionItem=document.createElement("div");
     TransactionItem.className="trans-item";
-
+    // p element inside item
     const newTransaction=document.createElement("p");
     newTransaction.className="item-name";
-    const Tname=document.createTextNode(tname);
-    newTransaction.appendChild(Tname);
-
+    const nameTextNode=document.createTextNode(tname);
+    newTransaction.appendChild(nameTextNode);
+    // amount element span
     const amount=document.createElement("span");
     amount.className="amount debit";
-    const Tamount=document.createTextNode(tamt);
-    amount.appendChild(Tamount);
+    const amountTextNode=document.createTextNode(tamt);
+    amount.appendChild(amountTextNode);
+    
+    // date element span
+    const TDate=document.createElement("span");
+    TDate.className="trans-date";
+    const dateTextNode=document.createTextNode(textDate);
+    TDate.appendChild(dateTextNode);
 
     newTransaction.appendChild(amount);
+    newTransaction.appendChild(TDate);
 
 
     TransactionItem.appendChild(newTransaction);
@@ -32,6 +44,7 @@ function newTransaction(tname, tamt) {
         <div class="trans-item">
             <p class="item-name">tname 
                 <span class="amount debit">tamt</span>
+                <span class="trans-date">textDate</span>
             </p>
         </div> 
     */
